0e635ec4401bf90e3521aaa00f2e49014804bbea
Vedant <vedant@example.com>
2026-03-25 21:52:37 +0530

    Remove useless files

--- fetch.py
+++
@@ -1,8 +0,0 @@
-from app import app
-app.testing = True
-with app.test_client() as c:
-    with c.session_transaction() as sess:
-        sess['logged_in'] = True
-    res = c.get('/admin?status=all')
-    with open('output_proper.html', 'w', encoding='utf-8') as f:
-        f.write(res.data.decode('utf-8'))
